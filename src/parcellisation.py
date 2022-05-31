c35fb72d3e921f3d942d73c7dab402ec70fe1d61
Sofian Chebboub <sofian.chebboub@epitech.eu>
2022-05-31 22:57:58 +0200

    [FIX] error when file is empty

--- src/parcellisation.py
+++ src/parcellisation.py
@@ -38,8 +38,11 @@ def parse_input_file(filepath: str):
     try:
         with open(filepath, "r", encoding="utf8") as file:
             lines: list(str) = file.readlines()
-    except FileNotFoundError:
+    except (FileNotFoundError):
         print("File not found")
+        sys.exit(84)
+    if len(lines) == 0:
+        print("File is empty")
         sys.exit(84)
     vertices_polygon = fill_list_vertices(lines)
 
@@ -128,6 +131,9 @@ class Field:
 
 
         def set_type(self, type):
+            """
+                Set the type of the cell
+            """
             self.type = type
 
 
