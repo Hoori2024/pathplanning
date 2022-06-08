7d0df3d381f2f5b697f26798b55f619778279711
Sofian Chebboub <sofian.chebboub@epitech.eu>
2022-06-08 22:21:41 +0200

    [Add] gestion error + correction pep8 norme

--- src/parcellisation.py
+++ src/parcellisation.py
@@ -1,5 +1,5 @@
-#/usr/bin/python3
-#encoding:utf-8
+# /usr/bin/python3
+# encoding:utf-8
 
 """
     pathplanning.py
@@ -10,7 +10,7 @@ from typing import List, Tuple
 from math import floor, ceil
 import sys
 
-from parsing import fill_list_edges
+from parsing import fill_list_edges, are_segments_secant
 
 
 Vertex = Tuple[float, float]
@@ -39,7 +39,8 @@ def parse_input_file(filepath: str) -> List[List[Vertex]]:
     # - vérifier que les polygones enfants soient tous dans le polygone parent
     # - vérifier qu'un polygone enfant ne soit pas dans un autre polygone enfant
 
-    polygons: List[List[Vertex]] = [] # List of polygons (polygon = list of its vertices)
+    # List of polygons (polygon = list of its vertices)
+    polygons: List[List[Vertex]] = []
     try:
         with open(filepath, "r", encoding="utf8") as file:
             lines: List[str] = file.readlines()
@@ -60,28 +61,9 @@ def parse_input_file(filepath: str) -> List[List[Vertex]]:
 def list_of_vertices_to_list_of_edges(vertices: List[Vertex]) -> List[Edge]:
     edges = []
     for idx_vertex in range(len(vertices)):
-        edges.append((vertices[idx_vertex], vertices[(idx_vertex + 1) % len(vertices)]))
+        edges.append(
+            (vertices[idx_vertex], vertices[(idx_vertex + 1) % len(vertices)]))
     return edges
-
-
-def are_segments_secant(A: Vertex, B: Vertex, C: Vertex, D: Vertex) -> Vertex:
-    """
-        Checks if the segments [AB] and [CD] are secant
-        Segments are on the form (x, y).
-        Returns the coordinates of the intersection, or None if not secant.
-    """
-    I = (B[0] - A[0], B[1] - A[1])
-    J = (D[0] - C[0], D[1] - C[1])
-    m = -1
-    k = -1
-    d = I[0] * J[1] - I[1] * J[0]
-
-    if d != 0:
-        m = (I[0] * A[1] - I[0] * C[1] - I[1] * A[0] + I[1] * C[0]) / d
-        k = (J[0] * A[1] - J[0] * C[1] - J[1] * A[0] + J[1] * C[0]) / d
-    if m >= 0 and m <= 1 and k >= 0 and k <= 1:
-        return (C[0] + m * J[0], C[1] + m * J[1])
-    return None
 
 
 def get_distance_between_segments(segment1, segment2) -> float:
@@ -92,19 +74,19 @@ def get_distance_between_segments(segment1, segment2) -> float:
 
 
 class Direction(Enum):
-            """
-                class Direction: represents directions that can be taken from a cell
-            """
-            UP = 0
-            RIGHT = 1
-            DOWN = 2
-            LEFT = 3
+    """
+        class Direction: represents directions that can be taken from a cell
+    """
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
 
-            def __str__(self):
-                return self.name
+    def __str__(self):
+        return self.name
 
-            def __repr__(self):
-                return self.name
+    def __repr__(self):
+        return self.name
 
 
 class Field:
@@ -116,7 +98,6 @@ class Field:
         """
             class Cell: cell of the field, expect position of the center of the cell
         """
-
 
         class CellType(Enum):
             """
@@ -133,7 +114,6 @@ class Field:
 
             def __repr__(self):
                 return self.name
-
 
         def __init__(self, center):
             """ center: the coordinates of the center """
@@ -154,7 +134,6 @@ class Field:
             for idx, edge in enumerate(edge_list):
                 self.edges.append((idx, edge))
 
-
         def __str__(self):
             string = f"Cell("
             string += f'center: {self.center}, '
@@ -163,17 +142,14 @@ class Field:
             string += ')'
             return string
 
-
         def __repr__(self):
             return str(self)
-
 
         def set_type(self, type):
             """
                 Set the type of the cell
             """
             self.type = type
-
 
     def __init__(self, polygons: List[List[Vertex]]):
         """
@@ -197,7 +173,7 @@ class Field:
                 elif i[0] > ceil(self.max_pos_x):
                     self.max_pos_x = ceil(i[0])
                 if i[1] < floor(self.min_pos_y):
-                     self.min_pos_y = floor(i[1])
+                    self.min_pos_y = floor(i[1])
                 elif i[1] > ceil(self.max_pos_y):
                     self.max_pos_y = ceil(i[1])
 
@@ -209,7 +185,6 @@ class Field:
 
         self.refresh_cells_types()
 
-
     def __str__(self):
         string = f"Edges: {self.edges}\n"
         string += "Cells:\n"
@@ -219,7 +194,6 @@ class Field:
             string += '---\n'
         return string
 
-
     def __repr__(self):
         string = f"edges: {self.edges}\n"
         string += "field:\n"
@@ -228,7 +202,6 @@ class Field:
                 string += (repr(cell) + '\n')
             string += '---\n'
         return string
-
 
     def refresh_cells_types(self):
         """
@@ -249,20 +222,20 @@ class Field:
                 else:
                     cell.set_type(cell.CellType.CENTER_ON_EDGE)
 
-
     def is_cell_center_in(self, cell):
         """
             check if a cell center is in or out of the edges
         """
         if self.count_secant_edge_with_segment((cell.center, (cell.center[0], self.max_pos_y))) % 2 == 1 or self.count_secant_edge_with_segment((cell.center, (self.max_pos_x, cell.center[1]))) % 2 == 1:
             for i in self.edges:
-                pos_sec_y = are_segments_secant(cell.center, (cell.center[0], self.max_pos_y), i[0], i[1])
-                pos_sec_x = are_segments_secant(cell.center, (self.max_pos_x, cell.center[1]), i[0], i[1])
+                pos_sec_y = are_segments_secant(
+                    cell.center, (cell.center[0], self.max_pos_y), i[0], i[1])
+                pos_sec_x = are_segments_secant(
+                    cell.center, (self.max_pos_x, cell.center[1]), i[0], i[1])
                 if pos_sec_y == cell.center or pos_sec_x == cell.center:
                     return None
             return True
         return False
-
 
     def count_secant_edge_with_segment(self, segment):
         """
@@ -274,18 +247,17 @@ class Field:
                 count += 1
         return count
 
-
     def is_cell_on_edge(self, cell):
         """
             check if a cell is on a edge of the field
         """
         for i in range(4):
             for j in self.edges:
-                pos = are_segments_secant(cell.vertices[i], cell.vertices[(i + 1) % 4], j[0], j[1])
+                pos = are_segments_secant(
+                    cell.vertices[i], cell.vertices[(i + 1) % 4], j[0], j[1])
                 if pos != None and pos[0] % 1 != 0 and pos[1] % 1 != 0:
                     return True
         return False
-
 
     def arrange_cells(self):
         """
@@ -303,8 +275,8 @@ class Field:
                 # `CENTER_INSIDE` and `COMPLETELY_INSIDE` cells)
                 surrounding_cells = self.get_surrounding_cells(line_nb, col_nb)
                 shift_ok_cells = filter(lambda cell: cell[1].type == self.Cell.CellType.COMPLETLY_INSIDE
-                    or cell.type == self.Cell.CellType.CENTER_INSIDE, surrounding_cells)
-                
+                                        or cell.type == self.Cell.CellType.CENTER_INSIDE, surrounding_cells)
+
                 # Getting the points of intersection with the field's edges:
                 pt_intersect = self.get_pt_intersect_for_cell(cell)
 
@@ -315,10 +287,10 @@ class Field:
         # Removing the cells completely outside:
         new_cells_list = []
         for line in self.cells:
-            filtered_line = filter(lambda cell: cell.type != self.Cell.CellType.COMPLETLY_OUTSIDE, line)
+            filtered_line = filter(
+                lambda cell: cell.type != self.Cell.CellType.COMPLETLY_OUTSIDE, line)
             new_cells_list.append(filtered_line)
         self.cells = new_cells_list
-
 
     def get_surrounding_cells(self, line_nb: int, col_nb: int) -> List[Tuple[Direction, Cell]]:
         """
@@ -328,15 +300,18 @@ class Field:
         """
         surrounding_cells = []
         if line_nb > 0:
-            surrounding_cells.append(Direction.UP, (self.cells[line_nb - 1][col_nb]))
+            surrounding_cells.append(
+                Direction.UP, (self.cells[line_nb - 1][col_nb]))
         if line_nb < len(self.cells) - 1:
-            surrounding_cells.append(Direction.DOWN, (self.cells[line_nb + 1][col_nb]))
+            surrounding_cells.append(
+                Direction.DOWN, (self.cells[line_nb + 1][col_nb]))
         if col_nb > 0:
-            surrounding_cells.append(Direction.LEFT, (self.cells[line_nb][col_nb - 1]))
+            surrounding_cells.append(
+                Direction.LEFT, (self.cells[line_nb][col_nb - 1]))
         if col_nb < len(self.cells[0]) - 1:
-            surrounding_cells.append(Direction.RIGHT, (self.cells[line_nb][col_nb + 1]))
+            surrounding_cells.append(
+                Direction.RIGHT, (self.cells[line_nb][col_nb + 1]))
         return surrounding_cells
-
 
     def get_pt_intersect_for_cell(self, cell: Cell) -> List[Tuple[Direction, Vertex]]:
         """
@@ -347,7 +322,8 @@ class Field:
         pt_intersect_list = []
         for (direction, cell_edge) in cell.edges:
             for field_edge in self.edges:
-                pt_intersect = are_segments_secant(cell_edge[0], cell_edge[1], field_edge[0], field_edge[1])
+                pt_intersect = are_segments_secant(
+                    cell_edge[0], cell_edge[1], field_edge[0], field_edge[1])
                 if pt_intersect is not None:
                     pt_intersect_list.append((direction, pt_intersect))
         return pt_intersect_list
--- src/parsing.py
+++ src/parsing.py
@@ -1,7 +1,7 @@
 # /usr/bin/python3
 # encoding:utf-8
 
-from parcellisation import are_segments_secant
+#from parcellisation import are_segments_secant
 
 """
     parsing.py (à modifier)
@@ -11,21 +11,45 @@ import sys
 
 from typing import Tuple, List
 
-
 Vertex = Tuple[float, float]
+Edge = Tuple[Vertex, Vertex]
 
 
-def fill_list_edges(lines):
+def are_segments_secant(p_a: Vertex, p_b: Vertex, p_c: Vertex, p_d: Vertex) -> Vertex:
     """
-        Fill list of edges verifing if there are not the same edge in file
+        Checks if the segments [AB] and [CD] are secant
+        Segments are on the form (x, y).
+        Returns the coordinates of the intersection, or None if not secant.
     """
-    polygons: list(list(Vertex)) = []
+    v_i: Tuple[float, float] = (p_b[0] - p_a[0], p_b[1] - p_a[1])
+    v_j: Tuple[float, float] = (p_d[0] - p_c[0], p_d[1] - p_c[1])
+    f_m: float = -1
+    f_k: float = -1
+    dist: float = v_i[0] * v_j[1] - v_i[1] * v_j[0]
+
+    if dist != 0:
+        f_m = (v_i[0] * p_a[1] - v_i[0] * p_c[1] -
+               v_i[1] * p_a[0] + v_i[1] * p_c[0]) / dist
+        f_k = (v_j[0] * p_a[1] - v_j[0] * p_c[1] -
+               v_j[1] * p_a[0] + v_j[1] * p_c[0]) / dist
+    if f_m >= 0 and f_m <= 1 and f_k >= 0 and f_k <= 1:
+        return (p_c[0] + f_m * v_j[0], p_c[1] + f_m * v_j[1])
+    return None
+
+
+def fill_list_edges(lines: List[str]):
+    """
+        Fill List of edges verifing if there are not the same edge in file
+        return List of edges as List[Edge]
+    """
+    polygons: List[Edge] = []
+
     for line in lines:
-        polygon_vertices: list(Vertex) = []
+        polygon_vertices: List[Vertex] = []
         line = line.replace("(", "").replace(" ", "").replace("\n", "")
         for elem in line.split(")"):
             if elem != "":
-                coords: list(str) = elem.split(";")
+                coords: List(str) = elem.split(";")
                 try:
                     new_vertex: Vertex = (float(coords[0]), float(coords[1]))
                     if polygon_vertices.count(new_vertex) > 0:
@@ -38,12 +62,14 @@ def fill_list_edges(lines):
     return polygons
 
 
-def get_list_sides(polygon_vertices):
+def get_list_sides(polygon_vertices: List[Vertex]) -> List[Edge]:
     """
-        Get list of sides of the polygon
+        Get List of sides of the polygon
+        return List of sides as List[Edge]
     """
-    list_sides: list(list(Vertex)) = []
-    for i in range(len(polygon_vertices)):
+    list_sides: List[Edge] = []
+
+    for i in enumerate(polygon_vertices):
         if i == len(polygon_vertices) - 1:
             list_sides.append([polygon_vertices[i], polygon_vertices[0]])
         else:
@@ -51,50 +77,140 @@ def get_list_sides(polygon_vertices):
     return list_sides
 
 
-def sides_connected(side1, side2):
+def sides_connected(side1: Edge, side2: Edge) -> bool:
     """
         Check if the side is connected to an other side
+        return True if they are connected
     """
     vertex1: Vertex = side1[0]
     vertex2: Vertex = side1[1]
     vertex3: Vertex = side2[0]
     vertex4: Vertex = side2[1]
     if vertex1[0] == vertex3[0] and vertex1[1] == vertex3[1]\
-            or vertex2[0] == vertex3[0] and vertex2[1] == vertex3[1]\
-            or vertex1[0] == vertex4[0] and vertex1[1] == vertex4[1]\
+            or vertex2[0] == vertex3[0] and vertex2[1] == vertex3[1]:
+        return True
+    if vertex1[0] == vertex4[0] and vertex1[1] == vertex4[1]\
             or vertex2[0] == vertex4[0] and vertex2[1] == vertex4[1]:
         return True
     return False
 
 
-def check_if_sides_are_not_segments(sides):
+def check_if_sides_are_not_segments(sides: List[Edge]) -> bool:
     """
         Check if the sides are not segments
-        Return True if they are not segments
+        return True if they are not segments
     """
-    for idx in range(len(sides)):
+    for idx in enumerate(sides):
         if idx == len(sides) - 1:
-            if not sides_connected(sides[idx], sides[0]) and are_segments_secant(sides[idx], sides[0]) != None:
+            if not sides_connected(sides[idx], sides[0])\
+                    and are_segments_secant(sides[idx][0], sides[idx][1],
+                                            sides[0][0], sides[0][1]) is not None:
                 return False
         else:
-            if not sides_connected(sides[idx], sides[idx + 1]) and are_segments_secant(sides[idx], sides[idx + 1]) != None:
+            if not sides_connected(sides[idx], sides[idx + 1])\
+                    and are_segments_secant(sides[idx][0], sides[idx][1],
+                                            sides[idx + 1][0], sides[idx + 1][1]) is not None:
                 return False
     return True
 
 
-def check_if_vertices_in_polygon(vertices_polygon, vertice):  # TODO: S
+def get_min_pos_y(polygon: List[Edge]) -> float:
     """
-        Check if the vertice is in the polygon
+        Get the minimum y of vertices of the polygon
+        return minimum y as float
     """
-    list(vertices_polygon)
-    list(vertice)
+    min_pos_y: float = polygon[0][0][1]
+    for edge in polygon:
+        for vertex in edge:
+            if vertex[1] < min_pos_y:
+                min_pos_y = vertex[1]
+    return min_pos_y
+
+
+def get_max_pos_y(polygon: List[Edge]) -> float:
+    """
+        Get the maximum y of vertices of the polygon
+        return maximum y as float
+    """
+    max_pos_y: float = polygon[0][0][1]
+    for edge in polygon:
+        for vertex in edge:
+            if vertex[1] > max_pos_y:
+                max_pos_y = vertex[1]
+    return max_pos_y
+
+
+def get_min_pos_x(polygon: List[Edge]) -> float:
+    """
+        Get the minimum x of vertices of the polygon
+        return minimum x as float
+    """
+    min_pos_x: float = polygon[0][0][0]
+    for edge in polygon:
+        for vertex in edge:
+            if vertex[0] < min_pos_x:
+                min_pos_x = vertex[0]
+    return min_pos_x
+
+
+def get_max_pos_x(polygon: List[Edge]) -> float:
+    """
+        Get the maximum x of vertices of the polygon
+        return maximum x as float
+    """
+    max_pos_x: float = polygon[0][0][0]
+    for edge in polygon:
+        for vertex in edge:
+            if vertex[0] > max_pos_x:
+                max_pos_x = vertex[0]
+    return max_pos_x
+
+
+def count_secant_edge_with_segment(polygon: List[Edge], segment: Tuple[Vertex, Vertex]):
+    """
+        Count the number of secant edges with a segment
+        return number of secant edges with a segment as int
+    """
+    count: int = 0
+
+    for edge in polygon:
+        if are_segments_secant(segment[0], segment[1], edge[0], edge[1]) is not None:
+            count += 1
+    return count
+
+
+def check_if_vertex_in_polygon(polygon: List[Edge], vertex: Vertex) -> bool:
+    """
+        Check if a vertex is in a polygon
+        return True if the vertex is in the polygon
+    """
+    # TODO: fix outside x only
+    max_pos_x: float = get_max_pos_x(polygon)
+    max_pos_y: float = get_max_pos_y(polygon)
+
+    if count_secant_edge_with_segment(polygon, (vertex, (vertex[0], max_pos_y))) % 2 == 1 or\
+            count_secant_edge_with_segment(polygon, (vertex, (max_pos_x, vertex[1]))) % 2 == 1:
+        for i in polygon:
+            pos_sec_y = are_segments_secant(
+                vertex, (vertex[0], max_pos_y), i[0], i[1])
+            pos_sec_x = are_segments_secant(
+                vertex, (max_pos_x, vertex[1]), i[0], i[1])
+            if vertex in (pos_sec_y, pos_sec_x):
+                return None
+        return True
     return False
 
 
-def check_if_child_in_parent(child, parent):  # TODO: S
+def check_if_child_in_parent(child: List[Edge], parent: List[Edge]) -> bool:
     """
         Check if the child is in the parent
+        return True if the child is in the parent
     """
-    list(child)
-    list(parent)
-    return False
+    for edge_c in child:
+        for vertex in edge_c:
+            if not check_if_vertex_in_polygon(parent, vertex):
+                return False
+        for edge_p in parent:
+            if not are_segments_secant(edge_c[0], edge_c[1], edge_p[0], edge_p[1]):
+                return False
+    return True
