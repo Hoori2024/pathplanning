3de563303d090514238a1b29ecf9e3dd6a69afab
richard <richard@pc-2.home>
2022-06-11 15:31:26 +0200

    [MOD] use shapes for visualisation

--- src/pathplanning/visualisation.py
+++ src/pathplanning/visualisation.py
@@ -1,18 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
-
+import matplotlib.patches as mpatches
 
 def display_field(field):
     """
         display the field
     """
-    array_to_display = []
+
+    colors = [['seagreen', "Cell completly inside"],
+    ['lightseagreen', "Cell center inside"],
+    ['yellow', "Cell center outside"],
+    ['gold', "Cell completly outside"],
+     ['deepskyblue', "Cell center on edge"]]
+
+    plt.axes()
+    for edge in field.edges:
+        line = plt.Line2D((edge[0][0], edge[1][0]), (edge[0][1], edge[1][1]), lw = 3, color='red')
+        plt.gca().add_line(line)
+
     for y in range(len(field.cells)):
-        array_to_display.append([])
         for x in range(len(field.cells[y])):
-            array_to_display[y].append(field.cells[y][x].type.value)
-    plt.pcolormesh(array_to_display, cmap="summer")
+            center = (field.cells[y][x].center[0] - 0.5, field.cells[y][x].center[1] - 0.5)
+            rectangle = plt.Rectangle(center , 1, 1, fc=colors[field.cells[y][x].type.value][0])
+            plt.gca().add_patch(rectangle)
+
+    legend = []
+    for elem in colors:
+        legend.append(mpatches.Patch(color=elem[0], label=elem[1]))
+    plt.legend(handles=legend, loc='lower left', bbox_to_anchor=(1.05, 0.95))
 
     plt.title('Field delimitation', fontweight ="bold")
+    plt.axis('scaled')
+    plt.tight_layout()
     plt.show()
